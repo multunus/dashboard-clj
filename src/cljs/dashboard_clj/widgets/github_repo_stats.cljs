3b757bdc1d0d94bdf24eba764cf65b5d23f9a571
Sreenath N <tacticiankerala@gmail.com>
2016-06-22 15:42:26 +0530

    Fix typo in github repo stats widget

--- src/cljs/dashboard_clj/widgets/github_repo_stats.cljs
+++ src/cljs/dashboard_clj/widgets/github_repo_stats.cljs
@@ -8,24 +8,20 @@
   [:div {:class "github-repo-stats"}
    [:div.header {:style {:background  (:color options)}}
     [:img {:class "github-logo" :src "/images/github_mark.png"}]
-    [:h2 {:class "title" }(str "Hello: " (:repo-name w))]]
+    [:h2 {:class "title" } (:repo-name w)]]
    [:div#total-commits
     [:p {:class "subheading"} "Total commits"]
     [:h4 {:class "total-commits" :style {:background (:color options)}} (get-in @data [:data :total-commits])]]
-    [:table {:class "contributors"}
-     [:tbody
-      [:tr
-       [:th {:class "subheading" }"contributors"]]
-      (doall (for [contributor (get-in @data [:data :contributors])]
-               [:tr {:key contributor}
-                [:td (str contributor)]]))]]
-   [:div {:class "commit-chart" :style { :width "95%" :height "40%"}} 
+   [:table {:class "contributors"}
+    [:tbody
+     [:tr
+      [:th {:class "subheading" }"contributors"]]
+     (doall (for [contributor (get-in @data [:data :contributors])]
+              [:tr {:key contributor}
+               [:td (str contributor)]]))]]
+   [:div {:class "commit-chart" :style { :width "95%" :height "40%"}}
     [charts/line-chart {:chart-options
                         {:title {:text text}
                          :xAxis {:categories (mapv first (get-in @data [:data :weekly-commit-breakdown]))}
                          :series [{:name "commits"
                                    :data (mapv second (get-in @data [:data :weekly-commit-breakdown]))}]}}]]])
-
-
-
-

ea1a94f77239a645c0a8e76ee24a46324684cb03
Sreenath N <tacticiankerala@gmail.com>
2016-06-24 10:20:16 +0530

    Deploy to clojars

--- docs/setting-up-dashboards.md
+++ docs/setting-up-dashboards.md
@@ -6,7 +6,7 @@
 
 With Leiningen, add
 ```clojure
-[dashboard-clj "0.1.0-SNAPSHOT"]
+[com.multunus/dashboard-clj "0.1.0-SNAPSHOT"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dashboard-clj "0.1.0-SNAPSHOT"
+(defproject com.multunus/dashboard-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

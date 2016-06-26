141081aa3f75ba7348b2ca79c8e499a59a3c55ff
Sreenath N <tacticiankerala@gmail.com>
2016-06-26 15:16:53 +0530

    Rename widget store

--- src/cljs/dashboard_clj/widgets/core.cljs
+++ src/cljs/dashboard_clj/widgets/core.cljs
@@ -2,13 +2,13 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :as rf]))
 
-(def widget (atom {}))
+(def widget-store (atom {}))
 
 (defn register-widget [name w]
-  (swap! widget assoc name w))
+  (swap! widget-store assoc name w))
 
 (defn setup-widget [{:keys [:data-source :type] :as w}]
   (if data-source
     (let [data (rf/subscribe [:app-db data-source])]
-      [(get @widget type) (merge w {:data data})])
-    ((get @widget type) w)))
+      [(get @widget-store type) (merge w {:data data})])
+    ((get @widget-store type) w)))

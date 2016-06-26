f9913ff3a03d24e74b3f4ee9cfec3e7e15ac1468
Sreenath N <tacticiankerala@gmail.com>
2016-06-26 20:50:08 +0530

    Fix description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.multunus/dashboard-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library to create dashboards"
+  :url "https://github.com/multunus/dashboard-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
